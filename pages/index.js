7fd7d817e86741a41f39e72adfe47a12ecb4709c
“Bruno <brunocostaribeiro@gmail.com>
2021-09-24 05:56:06 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>
-        Em Construção
-    </div>
+    return (
+    <div>
+        <h1>Home</h1>
+        
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>)
 }
 
 export default Home
