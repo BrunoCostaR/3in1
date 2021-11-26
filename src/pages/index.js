b0cf7ae68953fa7390f66bff3a056bf723c5f180
“Bruno <brunocostaribeiro@gmail.com>
2021-11-26 18:26:57 -0300

    VideoNoIndex

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
-import Head from 'next/head'
-//import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+//import BackVideo from "videos/video.mp4";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Triuno APP</title>
+        <title>3in1</title>
       </Head>
 
+      <video
+        className={styles.video}
+        autoPlay
+        loop
+        muted
+      >
+        <source src="videos/video.mp4" type="video/mp4" />
+      </video>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-        Triuno App
-        </h1>
-        <p className={styles.description}>
-          O
-          <code className={styles.code}>Código Fonte</code>
-          está em você
-        </p>
+        <h1 className={styles.title}>3in1.com.br</h1>
+        <p className={styles.description}>Porque o Mundo é Digital!</p>
         <div className={styles.grid}>
           <a href="" className={styles.card}>
             <h2>Portfólio &rarr;</h2>
@@ -29,7 +32,10 @@ export default function Home() {
             <p>O que já fizemos.</p>
           </a>
 
-          <a href="https://instagram.com/3in1.com.br" className={styles.card}>
+          <a
+            href="https://www.instagram.com/3in1.com.br/"
+            className={styles.card}
+          >
             <h2>Instagram &rarr;</h2>
             <p>O que estamos fazendo.</p>
           </a>
@@ -41,7 +47,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  )
+  );
 }
-
-
